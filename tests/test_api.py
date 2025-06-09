383fa398d54b78cd29c4766c68dc81f1708be45e
Chris Schmidt <p.christopher.schmidt@gmail.com>
2025-06-09 17:13:30 +0200

    Skip API tests without fastapi

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,7 @@ import pytest
 
 pytest.importorskip("httpx")
 pytest.importorskip("cvxpy")
+pytest.importorskip("fastapi")
 from fastapi.testclient import TestClient
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
